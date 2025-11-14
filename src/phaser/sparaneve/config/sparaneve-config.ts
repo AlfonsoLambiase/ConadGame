3d6cd5e025a1c05246b5e9a7f8b7950875286802
Jesus Giuseppe Antonio Mastrolonardo <jesusmastrolonardo@Mac.station>
2025-11-14 12:17:37 +0100

    feat(sparaneve): test landscape1

--- src/phaser/sparaneve/config/sparaneve-config.ts
+++ src/phaser/sparaneve/config/sparaneve-config.ts
@@ -1,26 +1,55 @@
+// export const GameSparaNeveConfig: Phaser.Types.Core.GameConfig = {
+//   //type: Phaser.CANVAS,
+//   type: Phaser.AUTO, // modificato per lo shader "snow" perche non funziona in canvas, creare un immagine fissa
+//   width: 1920,
+//   height: 1080,
+//   parent: "game-container",
+//   autoRound: false,
+//   scale: {
+//     mode: Phaser.Scale.ENVELOP, // Fit the game to the screen
+//     autoCenter: Phaser.Scale.CENTER_BOTH, // Center the game on the screen
+//     height: window.innerHeight * window.devicePixelRatio,
+//     width: window.innerWidth * window.devicePixelRatio,
+//   },
+//   physics: {
+//     default: "arcade",
+//     arcade: {
+//       gravity: {y: 0, x: 0},
+//       debug: false,
+//     },
+//   },
+//   transparent: true,
+//   input: {
+//     activePointers: 3, // Enable multitouch
+//   },
+//   scene: [],
+// };
+
+const w = Math.max(window.innerWidth, window.innerHeight) * window.devicePixelRatio;
+const h = Math.min(window.innerWidth, window.innerHeight) * window.devicePixelRatio;
+
 export const GameSparaNeveConfig: Phaser.Types.Core.GameConfig = {
-  //type: Phaser.CANVAS,
-  type: Phaser.AUTO, // modificato per lo shader "snow" perche non funziona in canvas, creare un immagine fissa
-  width: 1920,
-  height: 1080,
+  type: Phaser.AUTO,
+  width: w,
+  height: h,
   parent: "game-container",
   autoRound: false,
   scale: {
-    mode: Phaser.Scale.ENVELOP, // Fit the game to the screen
-    autoCenter: Phaser.Scale.CENTER_BOTH, // Center the game on the screen
-    height: window.innerHeight * window.devicePixelRatio,
-    width: window.innerWidth * window.devicePixelRatio,
+    mode: Phaser.Scale.ENVELOP,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: w,
+    height: h,
   },
   physics: {
     default: "arcade",
     arcade: {
-      gravity: {y: 0, x: 0},
+      gravity: { y: 0, x: 0 },
       debug: false,
     },
   },
   transparent: true,
   input: {
-    activePointers: 3, // Enable multitouch
+    activePointers: 3,
   },
   scene: [],
 };
